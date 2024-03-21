6590cd665b6a9f1d57b5509138037a79cd51fc99
Aidoer <r05921078@ntu.edu.tw>
2024-03-21 19:52:28 +0800

    Update css and img.

--- src/components/MessageContainer/CodeBlock.tsx
+++ src/components/MessageContainer/CodeBlock.tsx
@@ -1,12 +1,11 @@
 import SyntaxHighlighter from "react-syntax-highlighter"
 import { darcula } from "react-syntax-highlighter/dist/esm/styles/hljs"
 import { CodeBlockProps } from "../../types"
-import "../../css/botrun-code.css"
 
 export function CodeBlock({ match, children }: CodeBlockProps) {
   const language = match ? match[1] : "unkown"
   return (
-    <>
+    <div className={`code-style-container`}>
       <div className={`code-style-top`}>{language}</div>
       <div className={`code-style-body`}>
         <SyntaxHighlighter language={language} style={darcula}>
@@ -14,6 +13,6 @@ export function CodeBlock({ match, children }: CodeBlockProps) {
         </SyntaxHighlighter>
       </div>
       <div className={`code-style-bottom`}>Boosted by Botrun.ai</div>
-    </>
+    </div>
   )
 }
